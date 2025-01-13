0005af2d54909f77b39fee5f0c953b27ec01e00c
Stanislav Trendafilov <114089296+Stanislav-Trendafilov@users.noreply.github.com>
2025-01-13 19:38:54 +0200

    add

--- PolynomialCalculatorFMI/PolynomialCalculatorFMI/PolynomialCalculatorFMI.cpp
+++ PolynomialCalculatorFMI/PolynomialCalculatorFMI/PolynomialCalculatorFMI.cpp
@@ -193,10 +193,43 @@ void runPolynomialCalculations()
     enterPolynomialCoeff(polynomialDegree2, polynom2);
     printPolynomial(polynom2, polynomialDegree2);
 
-    if (function == 1)
+    switch (function)
     {
-        //  sumOfPolynomials(polynom1, polynom2,polynomialDegree,polynomialDegree2);
+        case 1:
+            sumOfPolynomials(polynom1, polynom2, polynomialDegree, polynomialDegree2);
+            break;
+        case 2:
+            
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+        case 6:
+
+            break;
+        case 7:
+
+            break;
+        case 8:
+
+            break;
+        case 9:
+
+            break;
+        case 10:
+
+            break;
+
+        default:
+            break;
     }
+
 }
 
 int main()
